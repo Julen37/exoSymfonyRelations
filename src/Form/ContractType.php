da5367d513f56f7d4df8edefe4e1721f3baf00ad
Julen37 <julen.gouchault@gmail.com>
2025-08-28 08:19:00 +0200

    crud contract

--- src/Form/ContractType.php
+++ src/Form/ContractType.php
@@ -10,7 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContractsType extends AbstractType
+class ContractType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
